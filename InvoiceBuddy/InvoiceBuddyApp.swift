2b9d1e71ea6e1874213e814f367527f23f80b29a
IulianB <iulian.bucatariu@gmail.com>
2025-03-05 23:21:07 +0100

    firstcommit

---
+++ InvoiceBuddy/InvoiceBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvoiceBuddyApp.swift
+//  InvoiceBuddy
+//
+//  Created by Iulian Bucatariu on 05.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct InvoiceBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InvoiceBuddyTests/InvoiceBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  InvoiceBuddyTests.swift
+//  InvoiceBuddyTests
+//
+//  Created by Iulian Bucatariu on 05.03.2025.
+//
+
+import Testing
+@testable import InvoiceBuddy
+
+struct InvoiceBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
